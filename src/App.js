4843dec0e43da819c497f839cf2ee45440b0706c
afozbek <furkanozbek1995@gmail.com>
2020-07-03 15:39:53 +0300

    useEffect kullanılarak componentlere veriler prop olarak atıldı

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import axios from './axios_intance';
 import Header from './components/Header';
+import CharacterList from './components/CharacterList';
 
 export const App = () => {
-  axios.get('/characters').then((res) => console.log(res.data));
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(`/characters`);
+
+      console.log(result.data);
+
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
 
   return (
     <div className="container">
       <Header />
+
+      <CharacterList isLoading={isLoading} items={items} />
     </div>
   );
 };
