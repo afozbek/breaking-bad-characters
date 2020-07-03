3e9c2ee8020da68875ceb00f2121dd451137ff8a
afozbek <furkanozbek1995@gmail.com>
2020-07-03 15:22:58 +0300

    Header componenti oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import axios from './axios_intance';
+import Header from './components/Header';
+
 export const App = () => {
-  return <div>Hello World</div>;
+  axios.get('/characters').then((res) => console.log(res.data));
+
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 };
